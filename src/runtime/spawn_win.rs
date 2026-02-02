3cfb6ab6ae3d6bba2980b92374190578214d566e
Groboclown <groboclown@gmail.com>
2026-02-01 19:16:27 -0600

    Start on windows support

--- src/runtime/spawn_win.rs
+++ src/runtime/spawn_win.rs
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: MIT
 
-/// Sandbox for Windows.
-/// 
-/// Inspired by the Chromium sandboxing model.
-/// Code:
-///   https://github.com/chromium/chromium/blob/main/sandbox
-///   under the BSD license.
-/// Documentation:
-///   https://github.com/chromium/chromium/blob/main/docs/design/sandbox.md
-///   https://github.com/chromium/chromium/blob/main/docs/design/sandbox_faq.md
+//! Sandbox for Windows.
+//!
+//! Inspired by the Chromium sandboxing model.
+//! Code:
+//!   https://github.com/chromium/chromium/blob/main/sandbox
+//!   under the BSD license.
+//! Documentation:
+//!   https://github.com/chromium/chromium/blob/main/docs/design/sandbox.md
+//!   https://github.com/chromium/chromium/blob/main/docs/design/sandbox_faq.md
